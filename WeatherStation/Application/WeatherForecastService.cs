8d0e874fd79efd8d227b5e4e9d9c5425410ccf33
Mantas Audickas <mantas@mentalist.dev>
2023-05-22 09:42:50 +0300

    Move business logic to application layer

---
+++ WeatherStation/Application/WeatherForecastService.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherStation.Application;
+
+public interface IWeatherForecastService
+{
+    IEnumerable<WeatherForecast> Get();
+}
+
+public class WeatherForecastService
+{
+    private static readonly string[] Summaries = {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+}
--- WeatherStation/Controllers/WeatherForecastController.cs
+++ WeatherStation/Controllers/WeatherForecastController.cs
@@ -1,32 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 
+using WeatherStation.Application;
+
 namespace WeatherStation.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly IWeatherForecastService _service;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(IWeatherForecastService service)
     {
-        _logger = logger;
+        _service = service;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return _service.Get();
     }
 }
